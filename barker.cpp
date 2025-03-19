90433a545d98b456f25db90063df6bb6505db28f
chinaza g ogwudiegwu <145911689+Nazacodes@users.noreply.github.com>
2025-03-19 00:44:56 +0000

    Update barker.cpp

--- barker.cpp
+++ barker.cpp
@@ -1,13 +1,30 @@
-const int micPin = A0;  // Connect the microphone to analog pin A0
-int micValue = 0;
+/*
+  Analog input, analog output, serial output
 
-void setup() {
-  Serial.begin(9600);  // Start serial communication
+  Reads an analog input pin, maps the result to a range from 0 to 255 and uses
+  the result to set the pulse width modulation (PWM) of an output pin.
+  Also prints the results to the Serial Monitor.
+*/
+
+// These constants won't change. They're used to give names to the pins used:
+const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
+const int analogOutPin = 9; // Analog output pin that the LED is attached to
+
+int sensorValue = 0;        // value read from the pot
+int outputValue = 0;        // value output to the PWM (analog out)
+
+void setup() {             
+  Serial.begin(9600);      // initialize serial communications at 9600 bps
 }
 
 void loop() {
-  micValue = analogRead(micPin);  // Read the microphone signal
-  Serial.println(micValue);  // Print the microphone value to Serial Monitor
-  
-  delay(100);  // Delay for a short time
+  sensorValue = analogRead(analogInPin);        // read the analog in value
+  outputValue = map(sensorValue, 0, 1023, 0, 255);       // map it to the range of the analog out
+  analogWrite(analogOutPin, outputValue);               // change the analog out value
+
+  Serial.println(outputValue);    // print the results to the Serial Monitor
+
+  // wait 2 milliseconds before the next loop for the analog-to-digital
+  // converter to settle after the last reading:
+  delay(2);
 }
